75b81f8156398855c817c6b4385c2bdda7ca6d96
feeshne <feeshne@gmail.com>
2022-06-17 16:32:31 +0800

    init profile

--- app/src/main/java/com/wentao/githubapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/wentao/githubapp/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.wentao.githubapp.ui.notifications
+package com.wentao.githubapp.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.wentao.githubapp.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -22,16 +22,16 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textProfile
+//        profileViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
